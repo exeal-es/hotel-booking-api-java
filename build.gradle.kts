667b2e70c0edff870fe7584240f1071d2b63ffa4
Pedro Pardal <ppardalj@gmail.com>
2024-11-19 08:17:24 +0100

    chore: add swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.h2database:h2")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testImplementation("io.rest-assured:rest-assured:5.5.0")
